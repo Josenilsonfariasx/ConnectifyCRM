4028187d228f237e9c1bed9d714761a677306931
Josenilsonfariasx <fariaslwork@gmail.com>
2024-01-23 01:17:55 -0300

    feat: adding route to login

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/Userclient/CreateUserControl
 import { ListUserController } from "./controllers/Userclient/ListClientController";
 import { DeleteUserController } from "./controllers/Userclient/DeleteUserController";
 import { UpdateUserController } from "./controllers/Userclient/UpdateUserController";
+import { AuthUserController } from "./controllers/Userclient/AuthUserController";
 const router = Router()
 
 router.get('/on', (req:Request, res:Response)=> {
@@ -14,5 +15,6 @@ router.post('/client', new CreateUserController().handle)
 router.get('/client', new ListUserController().handle)
 router.delete('/client/id', new DeleteUserController().handle)
 router.put('/client/id', new UpdateUserController().handle)
+router.post('/client/login', new AuthUserController().handle)
 
 export {router}
