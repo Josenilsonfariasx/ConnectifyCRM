02f9e6e32507a2c081f683deeca77cccc4bd8479
Josenilsonfariasx <fariaslwork@gmail.com>
2024-01-24 08:41:05 -0300

    feat: adding style in pdf

--- src/services/PDF/GetPdfService.ts
+++ src/services/PDF/GetPdfService.ts
@@ -1,6 +1,5 @@
 import prismaClient from "../../prisma";
 import PDFDocument from 'pdfkit';
-import { Request, Response } from "express";
 
 class GetPdfService {
   async execute(clientId: string): Promise<Buffer> {
@@ -26,26 +25,37 @@ class GetPdfService {
 
     const doc = new PDFDocument();
 
-    doc.fontSize(16).text('Relatório de Contatos do Cliente', { align: 'center' });
-    doc.moveDown();
+    doc.font('Helvetica-Bold').fontSize(20).text('Connectfy-CRM', { align: 'center' });
+    doc.moveDown(0.5);
+    
+    doc.moveDown(1);
+    doc.font('Helvetica').fontSize(16).text('Relatório de Contatos do Cliente', { align: 'center' });
+    doc.moveDown(1.5);
+
     list.forEach(dado => {
       doc.moveDown(1.5);
-      doc.font('Helvetica-Bold').fontSize(14).text(`ID: ${dado.id}`);
-      doc.font('Helvetica').fontSize(12).text(`Nome: ${dado.nomeCompleto}`);
-      doc.font('Helvetica').fontSize(12).text(`Email: ${dado.email}`);
-      doc.font('Helvetica').fontSize(12).text(`Telefone: ${dado.telefone}`);
-      doc.font('Helvetica').fontSize(12).text(`Data de Registro: ${dado.dataRegistro}`);
-      doc.font('Helvetica').fontSize(12).text(`ID do Cliente: ${dado.clientId}`);
-      if(dado.communications.length !== 0){
-        doc.font('Helvetica').fontSize(12).text(`Outras opções de contato: ${dado.communications}`);
+
+      doc.font('Helvetica-Bold').fontSize(14).text(`ID: ${dado.id}`, { color: 'navy' });
+      doc.font('Helvetica').fontSize(12).text(`Nome: ${dado.nomeCompleto}`, { color: 'darkgreen' });
+      doc.font('Helvetica').fontSize(12).text(`Email: ${dado.email}`, { color: 'darkgreen' });
+      doc.font('Helvetica').fontSize(12).text(`Telefone: ${dado.telefone}`, { color: 'darkgreen' });
+      doc.font('Helvetica').fontSize(12).text(`Data de Registro: ${dado.dataRegistro}`, { color: 'darkgreen' });
+      doc.font('Helvetica').fontSize(12).text(`ID do Cliente: ${dado.clientId}`, { color: 'darkgreen' });
+
+      if (dado.communications.length !== 0) {
+        doc.moveDown(0.5);
+        doc.font('Helvetica').fontSize(12).text(`Outras opções de contato: ${dado.communications}`, { color: 'darkblue' });
       }
-    doc.moveDown();
-      doc.font('Helvetica').fontSize(12).text('-'.repeat(117));
+
+      doc.moveDown();
+      doc.lineCap('butt').moveTo(50, doc.y).lineTo(550, doc.y).stroke();
+      doc.moveDown(1.5);
     });
 
+    doc.moveDown(2);
+    doc.font('Helvetica').fontSize(12).text('Obrigado por escolher a nossa empresa para seus serviços.', { align: 'center' });
 
     doc.end();
-
 
     return new Promise((resolve, reject) => {
       const chunks: any[] = [];
